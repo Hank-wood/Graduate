c46eca8fce4fb91135f0d8bf549a394429aa5ca8
laike9m <laike9m@gmail.com>
2015-12-14 13:58:58 +0800

    tmp

--- dynamic/db.py
+++ dynamic/db.py
@@ -5,13 +5,17 @@
 """
 
 from pymongo import MongoClient
+import ezcf
+
+from config.dynamic_config import topics
 
 
 class DB:
     def __init__(self):
         self.db = MongoClient('localhost', 27017).zhihu_data
+        for topic in topics:
+            self.__dict__[topic] = self.db[topic]
         self.user = self.db.user
-        self.answer = self.db.answer
 
     def find_user(self):
         pass
