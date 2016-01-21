215447335b5b9b29deffda9fcee447b2ddae9265
laike9m <laike9m@gmail.com>
2016-01-21 20:33:29 +0800

    follower<1000新用户入库

--- follower/follower.py
+++ follower/follower.py
@@ -9,19 +9,40 @@ db = MongoClient('127.0.0.1', 27017).zhihu_data
 user_coll = db.user
 
 
+def remove_all_users():
+    user_coll.remove({})
+
+
+def show_users():
+    pprint(list(user_coll.find()))
+
+
 def fetch_followers(uid, datetime):
     prefix = 'https://www.zhihu.com/people/'
     user = client.author(prefix + uid)
     uids = []
 
-    if user.follower_num > 1000:
-        fetch_many_followers(uid)
+    doc = user_coll.find_one({'uid': uid})
+    if doc is None:
+        # new user
+        if user.follower_num > 1000:
+            fetch_many_followers(uid)
+        else:
+            for follower in user.followers:
+                uids.append(follower.id)
+            pprint(uids)
+            assert len(uids) == user.follower_num
+            user_coll.insert({
+                'uid': uid,
+                "follower": [{
+                    'time': datetime,
+                    'uids': uids
+                }]
+            })
     else:
-        for follower in user.followers:
-            uids.append(follower.id)
-        pprint(uids)
-        assert len(uids) == user.follower_num
+        # TODO: insert newly added followers
+        pass
 
 
 def fetch_many_followers(uid):
-    pass
+    print("fetch followers > 1000")
--- follower/test/test_follower.py
+++ follower/test/test_follower.py
@@ -2,6 +2,16 @@ import follower
 from datetime import datetime
 
 
+def setup_function(function):
+    follower.remove_all_users()
+
+
 def test_fetch_few():
     now = datetime.now()
     follower.fetch_followers('aiwanxin', now)
+    follower.show_users()
+
+
+def test_fetch_many():
+    now = datetime.now()
+    follower.fetch_followers('laike9m', now)
