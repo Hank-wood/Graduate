2fba623dcd4c0873e0a31fdec986f0c69db937a4
laike9m <laike9m@gmail.com>
2016-01-25 20:18:49 +0800

    use partial for testing

--- dynamic/test/test_follower.py
+++ dynamic/test/test_follower.py
@@ -1,5 +1,6 @@
 import time
 from datetime import datetime, timedelta
+from functools import partial
 from unittest.mock import Mock, patch, PropertyMock
 
 import requests
@@ -11,20 +12,25 @@ from huey_tasks import _fetch_followers, show_users, remove_all_users, get_user
 from utils import dict_equal
 
 
+_fetch_followers = partial(_fetch_followers, db_name='test')
+get_user = partial(get_user, db_name='test')
+remove_all_users = partial(remove_all_users, db_name='test')
+show_users = partial(show_users, db_name='test')
+
 
 def setup_function(function):
-    remove_all_users(db_name='test')
+    remove_all_users()
 
 
 @pytest.mark.skipif(True, reason='')
 def test_fetch_few():
-    _fetch_followers('aiwanxin', datetime.now(), db_name='test')
+    _fetch_followers('aiwanxin', datetime.now())
     time.sleep(5)
     show_users('test')
 
 @pytest.mark.skipif(True, reason='')
 def test_fetch_many():
-    _fetch_followers('laike9m', datetime.now(), db_name='test')
+    _fetch_followers('laike9m', datetime.now())
     time.sleep(5)
     show_users('test')
 
@@ -40,24 +46,24 @@ def test_fetch_increased_followers(mock_followers, mock_follower_num):
 
     mock_follower_num.side_effect = [2,2,3,4]
     now = datetime.now()
-    _fetch_followers('laike9m', now, db_name='test')
-    doc = get_user('laike9m', db_name='test')
+    _fetch_followers('laike9m', now)
+    doc = get_user('laike9m')
     assert len(doc['follower']) == 1
     assert doc['follower'][0]['uids'] == ['a', 'b']
 
-    _fetch_followers('laike9m', now + timedelta(seconds=1), db_name='test')
-    doc = get_user('laike9m', db_name='test')
+    _fetch_followers('laike9m', now + timedelta(seconds=1))
+    doc = get_user('laike9m')
     assert len(doc['follower']) == 1
     assert doc['follower'][0]['uids'] == ['a', 'b']
 
-    _fetch_followers('laike9m', now + timedelta(seconds=2), db_name='test')
-    doc = get_user('laike9m', db_name='test')
+    _fetch_followers('laike9m', now + timedelta(seconds=2))
+    doc = get_user('laike9m')
     assert len(doc['follower']) == 2
     assert doc['follower'][0]['uids'] == ['a', 'b']
     assert doc['follower'][1]['uids'] == ['c']
 
-    _fetch_followers('laike9m', now + timedelta(seconds=3), db_name='test')
-    doc = get_user('laike9m', db_name='test')
+    _fetch_followers('laike9m', now + timedelta(seconds=3))
+    doc = get_user('laike9m')
     assert len(doc['follower']) == 3
     assert doc['follower'][0]['uids'] == ['a', 'b']
     assert doc['follower'][1]['uids'] == ['c']
