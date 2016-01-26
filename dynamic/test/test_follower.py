dfcfb2a8af56bdb4e1bc2fd7ba00be3aea04223b
laike9m <laike9m@gmail.com>
2016-01-26 18:39:18 +0800

    完善 test_follower

--- dynamic/test/test_follower.py
+++ dynamic/test/test_follower.py
@@ -33,43 +33,76 @@ def setup_function(function):
 def test_fetch_few():
     _fetch_followers(aiwanxin, datetime.now())
     _fetch_followees(aiwanxin, datetime.now())
-    time.sleep(5)
     show_users()
     doc = get_user('aiwanxin')
     assert len(doc['follower'][0]['uids']) >= 32
     assert len(doc['followee'][0]['uids']) >= 44
 
 
+# @pytest.mark.skipif(True, reason='')
 def test_fetch_few2():
     _fetch_followers_followees('aiwanxin', datetime.now())
-    time.sleep(5)
     show_users()
     doc = get_user('aiwanxin')
     assert len(doc['follower'][0]['uids']) >= 32
     assert len(doc['followee'][0]['uids']) >= 44
 
 
-@pytest.mark.skipif(True, reason='')
+# @pytest.mark.skipif(True, reason='')
 def test_fetch_many():
-    _fetch_followers(laike9m, datetime.now())
-    time.sleep(5)
+    # 多follower, 少followee
+    _fetch_followers_followees('laike9m', datetime.now())
+    doc = get_user('laike9m')
+    assert 'follower' not in doc
+    assert len(doc['followee'][0]['uids']) >= 332
+
+    # 多followee, 少follower
+    _fetch_followers_followees('yang-de-33-3', datetime.now())
+    doc = get_user('yang-de-33-3')
+    assert 'followee' not in doc
+    assert len(doc['follower'][0]['uids']) >= 15
+
     show_users()
-    doc = get_user('aiwanxin')
-    assert len(doc['follower']) == 1
-    assert doc['follower'][0]['uids'] == ['a', 'b']
 
 
-@pytest.mark.skipif(True, reason='')
+# @pytest.mark.skipif(True, reason='')
 @patch('huey_tasks.zhihu.Author.follower_num', new_callable=PropertyMock)
 @patch('huey_tasks.zhihu.Author.followers', new_callable=PropertyMock)
-def test_fetch_increased_followers(mock_followers, mock_follower_num):
-    mock_followers.side_effect = [
+@patch('huey_tasks.zhihu.Author.followee_num', new_callable=PropertyMock)
+@patch('huey_tasks.zhihu.Author.followees', new_callable=PropertyMock)
+def test_fetch_many2(ees, ee_num, ers, er_num):
+    # 测试 follower 和 followee 均超过 500 的情况
+    # follower_num < followwee_num
+    er_num.return_value = 501
+    ers.return_value  = [Mock(id='uid'+str(i)) for i in range(501)]
+    ee_num.return_value  = 502
+    ees.return_value = [Mock(id='uid'+str(i)) for i in range(502)]
+    _fetch_followers_followees('test_user', datetime.now())
+    doc = get_user('test_user')
+    assert 'followee' not in doc
+    assert len(doc['follower'][0]['uids']) == 501
+
+    # follower_num > followwee_num
+    er_num.return_value = 502
+    ers.return_value  = [Mock(id='uid'+str(i)) for i in range(502)]
+    ee_num.return_value  = 501
+    ees.return_value = [Mock(id='uid'+str(i)) for i in range(501)]
+    _fetch_followers_followees('test_user2', datetime.now())
+    doc = get_user('test_user2')
+    assert 'follower' not in doc
+    assert len(doc['followee'][0]['uids']) == 501
+
+
+#@pytest.mark.skipif(True, reason='')
+@patch('huey_tasks.zhihu.Author.follower_num', new_callable=PropertyMock)
+@patch('huey_tasks.zhihu.Author.followers', new_callable=PropertyMock)
+def test_fetch_increased_followers(ers, er_num):
+    ers.side_effect = [
         [Mock(id='a'), Mock(id='b')],
         [Mock(id='c'), Mock(id='a'), Mock(id='b')],
         [Mock(id='e'), Mock(id='d'), Mock(id='c'), Mock(id='b')], # a unfollowed
     ]
-
-    mock_follower_num.side_effect = [2,2,3,4]
+    er_num.side_effect = [2, 2, 3, 4]
     now = datetime.now()
     _fetch_followers(laike9m, now)
     doc = get_user('laike9m')
@@ -94,3 +127,37 @@ def test_fetch_increased_followers(mock_followers, mock_follower_num):
     assert doc['follower'][1]['uids'] == ['c']
     assert doc['follower'][2]['uids'] == ['e', 'd']
 
+
+#@pytest.mark.skipif(True, reason='')
+@patch('huey_tasks.zhihu.Author.followee_num', new_callable=PropertyMock)
+@patch('huey_tasks.zhihu.Author.followees', new_callable=PropertyMock)
+def test_increased_followees(ees, ee_num):
+    ees.side_effect = [
+        [Mock(id='a'), Mock(id='b')],
+        [Mock(id='c'), Mock(id='a'), Mock(id='b')],
+        [Mock(id='e'), Mock(id='d'), Mock(id='c'), Mock(id='b')], # a unfollowed
+    ]
+    ee_num.side_effect = [2, 2, 3, 4]
+    now = datetime.now()
+    _fetch_followees(laike9m, now)
+    doc = get_user('laike9m')
+    assert len(doc['followee']) == 1
+    assert doc['followee'][0]['uids'] == ['a', 'b']
+
+    _fetch_followees(laike9m, now + timedelta(seconds=1))
+    doc = get_user('laike9m')
+    assert len(doc['followee']) == 1
+    assert doc['followee'][0]['uids'] == ['a', 'b']
+
+    _fetch_followees(laike9m, now + timedelta(seconds=2))
+    doc = get_user('laike9m')
+    assert len(doc['followee']) == 2
+    assert doc['followee'][0]['uids'] == ['a', 'b']
+    assert doc['followee'][1]['uids'] == ['c']
+
+    _fetch_followees(laike9m, now + timedelta(seconds=3))
+    doc = get_user('laike9m')
+    assert len(doc['followee']) == 3
+    assert doc['followee'][0]['uids'] == ['a', 'b']
+    assert doc['followee'][1]['uids'] == ['c']
+    assert doc['followee'][2]['uids'] == ['e', 'd']
