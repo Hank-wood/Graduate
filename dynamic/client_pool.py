0bcdd58f396962bc04097ca8d4d58999046cb882
laike9m <laike9m@gmail.com>
2016-02-04 22:42:11 +0800

    把初始的 pool_maxsize 设置成1000防止warning:Connection pool is full

--- dynamic/client_pool.py
+++ dynamic/client_pool.py
@@ -1,8 +1,14 @@
 import zhihu
+from requests.adapters import HTTPAdapter, Retry
+
 from common import test_cookie
 
 
 _client1 = zhihu.ZhihuClient(test_cookie)
+_client1._session.mount('https://www.zhihu.com',
+                         HTTPAdapter(pool_connections=1,
+                                     pool_maxsize=1000,
+                                     max_retries=Retry(100)))
 
 
 def get_client():
