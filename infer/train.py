a2a332686e55a1e7134488bd2da273f55777dbcb
laike9m <laike9m@gmail.com>
2016-03-24 21:03:50 +0800

    在 alltime 数据上跑了一下 train/test

--- infer/train.py
+++ infer/train.py
@@ -7,7 +7,7 @@ import os
 import pickle
 
 import pymongo
-from sklearn import svm
+from sklearn import svm, cross_validation
 
 from feature import StaticAnswer
 from user import UserManager
@@ -16,25 +16,61 @@ from iutils import a_col
 db = pymongo.MongoClient('127.0.0.1', 27017).get_database('sg1')
 sys.modules['feature'].__dict__['db'] = db
 sys.modules['feature'].__dict__['user_manager'] = UserManager(db.user)
-clf = svm.SVC()
-
-features = []
-samples = []
-start = time.time()
 
 pickle_filename = 'data/feature.pkl'
 if os.path.exists(pickle_filename):
-    data = pickle.load(open(pickle_filename, 'rb'))
+    with open(pickle_filename, 'rb') as f:
+        data = pickle.load(f)
 else:
     data = {}
 
-with open('data/alltime.txt') as f:
-    for line in f:
-        tid, qid, _ = line.split(',', maxsplit=2)
-        a_collection = db[a_col(tid)]
-        aids = [a_doc['aid'] for a_doc in
-                a_collection.find({'qid': qid}, {'aid': 1})]
-        for aid in aids:
+
+def gen_traindata_selected():
+    """
+    返回 alltime 中的 question 的 answer 的 follow 关系 train data
+    :return: [features], [samples]
+    """
+    features = []
+    samples = []
+    with open('data/alltime.txt') as f:
+        for line in f:
+            tid, qid, _ = line.split(',', maxsplit=2)
+            a_collection = db[a_col(tid)]
+            aids = [a_doc['aid'] for a_doc in
+                    a_collection.find({'qid': qid}, {'aid': 1})]
+            for aid in aids:
+                answer = StaticAnswer(tid, aid)
+                if aid not in data:
+                    answer.load_from_dynamic()
+                    answer.build_cand_edges()
+                    target = answer.gen_target()
+                    f = answer.gen_features()
+                    data[aid] = {
+                        'edge': answer.cand_edges,
+                        'target': target,
+                        'feature': f
+                    }
+                    samples.extend(target)
+                    features.extend(f)
+                else:
+                    samples.extend(data[aid]['target'])
+                    features.extend(data[aid]['feature'])
+    return features, samples
+
+
+def gen_traindata_from_all():
+    """
+    生成数据库中全部数据的 features, samples
+    :return: [features] [samples]
+    """
+    features = []
+    samples = []
+    a_colls = ["19550517_a", "19551147_a", "19561087_a", "19553298_a"]
+    for a_coll in a_colls:
+        tid = a_coll[:-2]
+        a_collection = db.get_collection(a_coll)
+        for adoc in a_collection.find({}, {'aid': 1}):
+            aid = adoc['aid']
             answer = StaticAnswer(tid, aid)
             if aid not in data:
                 answer.load_from_dynamic()
@@ -51,11 +87,24 @@ with open('data/alltime.txt') as f:
             else:
                 samples.extend(data[aid]['target'])
                 features.extend(data[aid]['feature'])
+    return features, samples
 
-pickle.dump(data, open(pickle_filename, 'wb'))
 
-print(len(features))
-print(len(samples))
-print(time.time() - start)
-print(clf.fit(features, samples))
-print(time.time() - start)
+def train():
+    clf = svm.SVC()
+    features, samples = gen_traindata_selected()
+    print(len(features))
+    print(len(samples))
+    with open(pickle_filename, 'wb') as f:
+        pickle.dump(data, f)
+
+    X_train, X_test, y_train, y_test = cross_validation.train_test_split(
+        features, samples, test_size=0.4, random_state=0)
+    clf.fit(X_train, y_train)
+    print(clf.score(X_test, y_test))
+
+    with open('data/model.pkl', 'wb') as f:
+        pickle.dump(clf, f)
+
+
+train()
