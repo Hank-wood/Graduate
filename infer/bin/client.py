617e629f5fc0d71c9f7332ba2f6ad5bae5bdf913
laike9m <laike9m@gmail.com>
2016-04-21 20:12:43 +0800

    add fetch_fo.py

--- infer/bin/client.py
+++ infer/bin/client.py
@@ -2,3 +2,16 @@ from zhihu import ZhihuClient
 
 cookie = '../../cookies/zhuoyi.json'
 client = ZhihuClient(cookie)
+"""
+import os
+from zhihu_oauth import ZhihuClient
+
+
+TOKEN_FILE = 'token.pkl'
+client = ZhihuClient()
+if os.path.isfile(TOKEN_FILE):
+    client.load_token(TOKEN_FILE)
+else:
+    client.login_in_terminal()
+    client.save_token(TOKEN_FILE)
+"""
--- infer/bin/fetch_fo.py
+++ infer/bin/fetch_fo.py
@@ -1,5 +1,6 @@
 """
-抓取没有follower or followee的, 从当前目录下的 tobe_fetch.txt 提取
+抓取没有follower or followee的
+fetch_from_file: 从当前目录下的 tobe_fetch.txt 提取
 """
 import re
 from client import client
@@ -11,36 +12,53 @@ db = pymongo.MongoClient('127.0.0.1', 27017).get_database('zhihu_data_0315')
 user_col = db.user
 user_manager = UserManager(db.user)
 
-with open('tobe_fetch.txt') as f:
-    lines = f.readlines()
 
-user_lack_follower = []
-user_lack_followee = []
+def fetch_from_file():
+    with open('tobe_fetch.txt') as f:
+        lines = f.readlines()
 
-pattern = re.compile(r'([^\s]+)\slacks follower,([^\s]+)\s')
-for line in lines:
-    obj = pattern.search(line)
-    user_lack_follower.append(obj.group(1))
-    user_lack_followee.append(obj.group(2))
+    user_lack_follower = []
+    user_lack_followee = []
 
-for er, ee in zip(user_lack_follower, user_lack_followee):
-    u1 = client.Author('https://www.zhihu.com/people/' + ee)
-    u2 = client.Author('https://www.zhihu.com/people/' + er)
-    if u1.followee_num < u2.follower_num:
-        udoc = user_col.find_one({'uid': ee})
+    pattern = re.compile(r'([^\s]+)\slacks follower,([^\s]+)\s')
+    for line in lines:
+        obj = pattern.search(line)
+        user_lack_follower.append(obj.group(1))
+        user_lack_followee.append(obj.group(2))
+
+    for er, ee in zip(user_lack_follower, user_lack_followee):
+        u1 = client.People('https://www.zhihu.com/people/' + ee)
+        u2 = client.People('https://www.zhihu.com/people/' + er)
+        if u1.followee_num < u2.follower_num:
+            udoc = user_col.find_one({'uid': ee})
+            assert udoc is not None
+            if 'followee' in udoc:
+                print("skip " + ee)
+                continue
+            print("fetch " + ee)
+            user_manager.fetch_user_followee(u1)
+        else:
+            udoc = user_col.find_one({'uid': er})
+            assert udoc is not None
+            if 'follower' in udoc:
+                print("skip " + er)
+                continue
+            print("fetch " + er)
+            user_manager.fetch_user_follower(u2)
+
+
+def fetch_followee_from_list(uid_list):
+    for uid in uid_list:
+        u = client.Author('https://www.zhihu.com/people/' + uid)
+        udoc = user_col.find_one({'uid': uid})
         assert udoc is not None
         if 'followee' in udoc:
-            print("skip " + ee)
+            print("skip " + uid)
             continue
-        print("fetch " + ee)
-        followees = user_manager.fetch_user_followee(u1)
-    else:
-        udoc = user_col.find_one({'uid': er})
-        assert udoc is not None
-        if 'follower' in udoc:
-            print("skip " + er)
-            continue
-        print("fetch " + er)
-        followers = user_manager.fetch_user_follower(u2)
+        print("fetch " + uid)
+        user_manager.fetch_user_followee(u)
 
+
+if __name__ == '__main__':
+    fetch_followee_from_list(['hai-xiao-ai'])
 
